744ce157e631e16bed48f8a1df9c9c8275b36fa2
Michal Andraka <michal.andraka@sidnet.info>
2021-06-07 16:07:04 +0200

    Database server installation

--- distro/ubuntu.sh
+++ distro/ubuntu.sh
@@ -19,7 +19,7 @@ get_apache_version() {
 }
 
 install_apache() {
-    apt-get update > /dev/null
+    apt-get update > /dev/null # TODO: should be executed once at the start of installation
     ln -fs /usr/share/zoneinfo/Europe/Warsaw /etc/localtime # to skip interactive installation
     apt-get install -y apache2  2>&1 | frame_output
 
@@ -40,4 +40,38 @@ is_mariadb_installed() {
 
 get_mariadb_version() {
     echo $(apt-cache show mariadb-server | grep '^Version' | sed 's/^.*\?:\s\+//')
+}
+
+install_mariadb() {
+    local ROOT_PASSWORD=$(generate_password)
+
+    (
+        apt-get install -y mariadb-server 2>&1
+        service mysql start 2>&1
+        service mysql enable 2>&1
+        echo -e "\n\n${ROOT_PASSWORD}\n${ROOT_PASSWORD}\n\n\n\nn\n\n" \
+            | /usr/bin/mysql_secure_installation 2>&1
+    ) | frame_output
+
+    local STATUS=${PIPESTATUS[0]}
+
+    if [ ${STATUS} = 0 ]; then
+        # Set variable so that the user doesn't need to enter this password
+        # for database creation step
+        MARIADB_ROOT_PASSWORD="${ROOT_PASSWORD}"
+        echo
+        print_notice "The installer has set the MariaDB
+            ${COLOR_NOTICE_BOLD}root${COLOR_NOTICE} user password to
+            \"${COLOR_NOTICE_BOLD}${ROOT_PASSWORD}${COLOR_NOTICE}\" (without the
+            quotes). Please keep this password safe or change it."
+        # TODO: Tell the user how to change the password or what docs to read
+        echo
+
+        print_centered "Press $(style_key Enter) to proceed." 80
+        echo
+
+        expect_enter
+    fi
+
+    return "${STATUS}"
 }
